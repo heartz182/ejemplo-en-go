dd7fad7a7126556782fa9da1fd53636795e7312f
heartz182 <eduardo.reyes182@hotmail.com>
2022-10-20 11:03:12 -0500

    funciones de cilindro, esfera y cono

--- volumen.go
+++ volumen.go
@@ -6,6 +6,13 @@ import (
 )
 
 func main() {
+
+	cilindro()
+	esfera()
+	cono()
+}
+
+func cilindro() {
 	var r, h float64
 
 	pi := math.Pi
@@ -18,6 +25,35 @@ func main() {
 
 	vol := (pi * (r * r)) * h
 
-	fmt.Println("El volumen es:", vol)
+	fmt.Println("El volumen del cilindro es:", vol)
 
+}
+
+func esfera() {
+	var r float64
+
+	pi := math.Pi
+
+	fmt.Println("Ingrese el radio (r)")
+	fmt.Scanf("%f", &r)
+
+	vol := ((4 * pi) * (r * r * r)) / 3
+
+	fmt.Println("El volumen de la esfera es:", vol)
+}
+
+func cono() {
+	var r, h float64
+
+	pi := math.Pi
+
+	fmt.Println("Ingrese el radio (r)")
+	fmt.Scanf("%f", &r)
+
+	fmt.Println("Ingrese la altura (h)")
+	fmt.Scanf("%f", &h)
+
+	vol := (pi * (r * r) * h) / 3
+
+	fmt.Println("El volumen del cono es:", vol)
 }
