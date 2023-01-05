83a7093903cc5234f8ea58e279424a5b3a6a0b91
heartz182 <eduardo.reyes182@hotmail.com>
2023-01-04 18:31:35 -0600

    tipos de datos

---
+++ tipos.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var edad uint8
+
+	edad = 30
+
+	fmt.Println("la edad es: ", edad, "años")
+
+	var peso float32
+	peso = 68.5
+
+	var altura float64
+	altura = 1.65
+
+	fmt.Println("el peso es", peso, "kg")
+	fmt.Println("la altura es", altura, "cm")
+}
+
+/* tipos numericos: enteros y flotantes*/
