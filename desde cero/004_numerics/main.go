e3b62b6806b35d55cbadefbb1ff4505d495225e9
heartz182 <eduardo.reyes182@hotmail.com>
2023-01-09 17:07:59 -0600

    ejercicios

---
+++ desde cero/004_numerics/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var edad uint8
+
+	edad = 24
+	fmt.Println("la edad es:", edad, "años")
+
+	var peso, altura float64
+
+	peso = 80.5
+	altura = 1.82
+
+	fmt.Println("El peso de la persona es:", peso, "kg")
+	fmt.Println("La altura de la persona es:", altura, "mtsls")
+
+}
