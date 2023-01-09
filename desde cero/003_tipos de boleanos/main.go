b3c595c57284000e689ef6405af3b4ae3a087d14
heartz182 <eduardo.reyes182@hotmail.com>
2023-01-09 16:44:53 -0600

    estrucutrando los ejercicios

---
+++ desde cero/003_tipos de boleanos/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Declara una variable b de tipo bool y la inicializa con el valor predeterminado (false)
+	var b bool
+	// Imprime el valor de b en la consola
+	fmt.Println(b)
+}
