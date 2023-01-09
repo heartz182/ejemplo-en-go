e956f33bc0772378a01b6beb0f80c7d8f7f89556
heartz182 <eduardo.reyes182@hotmail.com>
2023-01-09 16:45:44 -0600

    archivos modificados

--- desde cero/002_tipos de datos/main.go
+++ desde cero/002_tipos de datos/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import "fmt"
 
+// hola devuelve "Hola" seguido de la cadena de entrada s
+func hola(s string) string {
+	return "Hola " + s
+}
+
+func main() {
+	// Imprime "Hola Eduardo" en la consola
+	fmt.Println(hola("Eduardo"))
 }
